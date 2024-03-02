4804909f3a717432fc7dd1053131538405b09fa9
anilyavas <anil.yavas99@gmail.com>
2024-03-02 13:59:06 +0300

    general structure and types

---
+++ src/content/post1.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'post1',
+  date: '2024-02-28T13:27:28Z',
+  title: 'My First Post',
+  description: 'This is my first post',
+  thumbnail: '',
+  content: `
+  
+  `,
+};
+
+export default post;
---
+++ src/content/post2.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'Post2',
+  date: '2024-02-28T13:27:28Z',
+  title: 'This is my second post',
+  description: 'Description: sadsadasThis is my post2',
+  thumbnail: '',
+  content: `
+  
+  `,
+};
+
+export default post;
